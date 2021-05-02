585b651d1d30b35d97535bce39736dad7cefd313
techlord001 <45688942+techlord001@users.noreply.github.com>
2021-05-02 09:34:49 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Measurement Conversion</title>
+  <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+  <div id="main-content">
+
+    <h1>Measurement Conversion</h1>
+
+    <ul id="measurement-types">
+      <li><a href="length.php">Length and distance</a></li>
+      <li><a href="area.php">Area</a></li>
+      <li><a href="volume.php">Volume and capacity</a></li>
+      <li><a href="mass.php">Mass and weight</a></li>
+      <li><a href="speed.php">Speed</a></li>
+      <li><a href="temperature.php">Temperature</a></li>
+    </ul>
+
+  </div>
+</body>
+
+</html>
---
+++ length.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Convert Length</title>
+  <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+  <div id="main-content">
+
+    <h1>Convert Length</h1>
+
+    <form action="" method="post">
+
+      <div class="entry">
+        <label>From:</label>&nbsp;
+        <input type="text" name="from_value" value="" />&nbsp;
+        <select name="from_unit">
+          <option>From Unit</option>
+        </select>
+      </div>
+
+      <div class="entry">
+        <label>To:</label>&nbsp;
+        <input type="text" name="to_value" value="" />&nbsp;
+        <select name="to_unit">
+          <option>To Unit</option>
+        </select>
+
+      </div>
+
+      <input type="submit" value="Submit" />
+    </form>
+
+    <br />
+    <a href="index.php">Return to menu</a>
+
+  </div>
+</body>
+
+</html>
