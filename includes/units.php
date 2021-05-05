3be97c06086fc8cf79502da4eab4bf2d449c0c44
techlord001 <45688942+techlord001@users.noreply.github.com>
2021-05-05 11:35:19 +0100

    adding volume units

--- includes/units.php
+++ includes/units.php
@@ -1,5 +1,5 @@
 <?php
-	const LENGTHS = array(
+	const UNITS = array(
 		"inches" => 0.0254,
 		"feet" => 0.3048,
 		"yards" => 0.9144,
@@ -11,4 +11,27 @@
 		"acres" => 1000,
 		"hectares" => 1000,
 	);
+
+	const VOLUME_UNITS = array(
+		"cubicInches" => 0.0163871,
+		"cubicFeet" => 28.3168,
+		"cubicCentimeters" => 0.001,
+		"cubicMeters" => 1000,
+		"ukGallons" => 4.54609,
+		"ukQuarts" => 1.13652,
+		"ukPints" => 0.568261,
+		"ukCups" => 0.284131,
+		"ukOunces" => 0.0284131,
+		"ukTablespoons" => 0.0177582,
+		"ukTeaspoons" => 0.00591939,
+		"usGallons" => 3.78541,
+		"usQuarts" => 0.946353,
+		"usPints" => 0.473176,
+		"usCups" => 0.24,
+		"usOunces" => 0.0295735,
+		"usTablespoons" => 0.0147868,
+		"usTeaspoons" => 0.00492892,
+		"liters" => 1,
+		"milliliters" => 0.001,
+	);  
 ?>
