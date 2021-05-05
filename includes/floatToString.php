38c8c08711e9f54e4a26045d65a061b32fc759bd
techlord001 <45688942+techlord001@users.noreply.github.com>
2021-05-05 13:54:54 +0100

    adding floatToString function to convert scientific notation to float

---
+++ includes/floatToString.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+*
+* Function to convert scientic notation of a very large or
+* very small number into a readable float */
+	function floatToString($float, $precision = 20)
+		{
+			$float = (float) $float; // Confirm variable is float
+			$string = number_format($float, $precision, '.', ''); // Format number
+			$string = rtrim(rtrim($string, '0'), '.'); // Remove any trailing zeros or decimal
+
+			return $string;
+		}
+?>
